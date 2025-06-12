d61b327b33549710dd17dda596f6f020d96b38b1
Mack Cooper <mackcooper1408@gmail.com>
2025-06-12 11:37:21 -0700

    add ball object class

---
+++ Glitter/Headers/ball_object.cpp
@@ -0,0 +1,44 @@
+#include "ball_object.hpp"
+
+BallObject::BallObject()
+    : GameObject(), Radius(12.5f), Stuck(true) {}
+
+BallObject::BallObject(glm::vec2 pos, float radius, glm::vec2 velocity,
+                       Texture2D sprite)
+    : GameObject(pos, glm::vec2(radius * 2.0f), sprite, glm::vec3(1.0f), velocity),
+      Radius(radius), Stuck(true) {}
+
+glm::vec2 BallObject::Move(float dt, unsigned int window_width)
+{
+  // if not stuck, move the ball
+  if (!this->Stuck)
+  {
+    // move the ball
+    this->Position += this->Velocity * dt;
+
+    // keep the ball inside the window bounds
+    if (this->Position.x <= 0.0f)
+    {
+      this->Position.x = 0.0f;
+      this->Velocity.x = -this->Velocity.x;
+    }
+    else if (this->Position.x + this->Size.x >= window_width)
+    {
+      this->Position.x = window_width - this->Size.x;
+      this->Velocity.x = -this->Velocity.x;
+    }
+    if (this->Position.y <= 0.0f)
+    {
+      this->Position.y = 0.0f;
+      this->Velocity.y = -this->Velocity.y;
+    }
+  }
+  return this->Position;
+}
+
+void BallObject::Reset(glm::vec2 position, glm::vec2 velocity)
+{
+  this->Position = position;
+  this->Velocity = velocity;
+  this->Stuck = true;
+}
---
+++ Glitter/Headers/ball_object.hpp
@@ -0,0 +1,25 @@
+#ifndef BALLOBJECT_HPP
+#define BALLOBJECT_HPP
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include "game_object.hpp"
+
+class BallObject : public GameObject
+{
+public:
+  // Ball properties
+  float Radius;
+  bool Stuck;
+
+  // Constructor
+  BallObject();
+  BallObject(glm::vec2 pos, float radius, glm::vec2 velocity, Texture2D sprite);
+
+  // Move the ball, keeping it inside the window bounds
+  glm::vec2 Move(float dt, unsigned int window_width);
+  void Reset(glm::vec2 position, glm::vec2 velocity);
+};
+
+#endif // BALLOBJECT_HPP
